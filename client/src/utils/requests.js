87c6a90550fea1032029e6e26a140870e8bb8feb
johaortiz <surf234@outlook.es>
2023-01-30 19:26:45 -0300

    railway add front

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -1,44 +1,49 @@
 import axios from "axios";
 
+const localhost = "http://localhost:3002";
+const railway = "https://pidogs-production-a8e4.up.railway.app";
+
 export const getAllDogs = () => {
-    const dogs = axios.get('http://localhost:3001/dogs');
-    return dogs;
+  const dogs = axios.get(`${railway}/dogs`);
+  return dogs;
 };
 
 export const getDogByName = (name) => {
-    const dog = axios.get(`http://localhost:3001/dogs/${name}`);
-    return dog;
+  const dog = axios.get(`${railway}/dogs/${name}`);
+  return dog;
 };
 
 export const getTemperaments = () => {
-    const temperament = axios.get('http://localhost:3001/temperaments');
-    return temperament;
+  const temperament = axios.get(`${railway}/temperaments`);
+  return temperament;
 };
 
 export const getDogById = (id) => {
-    const dog = axios.get(`http://localhost:3001/dogs/detail/${id}`);
-    return dog;
+  const dog = axios.get(`${railway}/dogs/detail/${id}`);
+  return dog;
 };
 
 export const postDogs = (dog, temperaments) => {
-    const options = {
-        method: 'POST',
-        url: 'http://localhost:3001/dogs',
-        data: {
-            name: dog.name,
-            image: dog.image,
-            weightMin: dog.weightMin,
-            weightMax: dog.weightMax,
-            height: `${dog.heightMin} - ${dog.heightMax}`,
-            life_span: `${dog.life_spanMin} - ${dog.life_spanMax}`,
-            temperaments
-        }
-    };
+  const options = {
+    method: "POST",
+    url: `${railway}/dogs`,
+    data: {
+      name: dog.name,
+      image: dog.image,
+      weightMin: dog.weightMin,
+      weightMax: dog.weightMax,
+      height: `${dog.heightMin} - ${dog.heightMax}`,
+      life_span: `${dog.life_spanMin} - ${dog.life_spanMax}`,
+      temperaments,
+    },
+  };
 
-    axios.request(options).then((response) => {
-        console.log(response.data);
-    }).catch((error) => {
-        console.error(error);
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
     });
-
-}
+};
